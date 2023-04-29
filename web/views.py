8597902fea5e1cf8a9bf6f2dbe2a30e65473be3e
Eduardo Cano <canoeduardojesus@gmail.com>
2023-04-29 20:23:10 -0300

    modulo 4

--- web/views.py
+++ web/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render,get_object_or_404,redirect
 
-from .models import Categoria,Producto
+from django.urls import reverse
+
+from .models import Categoria,Producto,Cliente,Pedido,PedidoDetalle
+
+# from paypal.standard.forms import PayPalPaymentsForm
+
+from django.conf import settings
+
 
 # Create your views here.
 """ VISTAS PARA EL CATALOGO DE PRODUCTOS """
-
 def index(request):
     listaProductos = Producto.objects.all()
     listaCategorias = Categoria.objects.all()
@@ -16,6 +22,7 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
 
 """ vista para filtrar productos por categorias"""
 def productosPorCategoria(request,categoria_id):
@@ -31,6 +38,7 @@ def productosPorCategoria(request,categoria_id):
     
     return render(request,'index.html',context)
 
+
 """ vista para filtrado de productos por nombre """
 def productosPorNombre(request):
     nombre = request.POST['nombre']
@@ -45,6 +53,7 @@ def productosPorNombre(request):
     
     return render(request,'index.html',context)
 
+
 """ vista para el detalle de producto"""
 def productoDetalle(request,producto_id):
     
@@ -57,11 +66,13 @@ def productoDetalle(request,producto_id):
     
     return render(request,'producto.html',context)
 
+
 """ VISTAS PARA EL CARRITO DE COMPRAS """
 from .carrito import Cart
 
 def carrito(request):
     return render(request,'carrito.html')
+
 
 def agregarCarrito(request,producto_id):
     if request.method == 'POST':
@@ -80,12 +91,14 @@ def agregarCarrito(request,producto_id):
     
     return render(request,'carrito.html')
 
+
 def eliminarProductoCarrito(request,producto_id):
     objProducto = Producto.objects.get(pk=producto_id)
     carritoProducto = Cart(request)
     carritoProducto.delete(objProducto)
     
     return render(request,'carrito.html')
+
 
 def limpiarCarrito(request):
     carritoProducto = Cart(request)
@@ -94,4 +107,257 @@ def limpiarCarrito(request):
     return render(request,'carrito.html')
 
 
+""" VISTAS PARA CLIENTES Y USUARIOS """
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
 
+from .forms import ClienteForm
+
+
+def crearUsuario(request):
+    
+    if request.method == 'POST':
+        dataUsuario = request.POST['nuevoUsuario']
+        dataPassword = request.POST['nuevoPassword']
+        
+        nuevoUsuario = User.objects.create_user(username=dataUsuario,password=dataPassword)
+
+        if nuevoUsuario is not None:
+            login(request,nuevoUsuario)
+            return redirect('/cuenta')
+    
+    
+    return render(request,'login.html')
+
+
+def loginUsuario(request):
+    paginaDestino = request.GET.get('next',None)
+
+    context = {
+        'destino':paginaDestino
+    }
+    
+    if request.method == 'POST':
+        dataUsuario = request.POST['usuario']
+        dataPassword = request.POST['password']
+        dataDestino = request.POST['destino']
+        
+        usuarioAuth = authenticate(request,username=dataUsuario,password=dataPassword)
+
+        if usuarioAuth is not None:
+            login(request,usuarioAuth)
+            
+            if dataDestino != 'None':
+                return redirect(dataDestino)
+            
+            return redirect('/cuenta')
+        
+        else:
+            context = {
+                'mensajeError':'Datos incorrectos'
+            }
+    
+    return render(request,'login.html',context)
+
+
+def logoutUsuario(request):
+    logout(request)
+    return render(request,'login.html')
+
+@login_required(login_url='/login')
+def cuentaUsuario(request):
+    
+    try:
+        clienteEditar = Cliente.objects.get(usuario = request.user)
+        
+        dataCliente = {
+            'nombre':request.user.first_name,
+            'apellidos':request.user.last_name,
+            'email':request.user.email,
+            'direccion':clienteEditar.direccion,
+            'telefono':clienteEditar.telefono,
+        }
+
+    except:
+        dataCliente = {
+        'nombre':request.user.first_name,
+        'apellidos':request.user.last_name,
+        'email':request.user.email
+        }
+    
+    frmCliente = ClienteForm(dataCliente)
+
+    context = {
+        'frmCliente':frmCliente
+    }
+    
+    return render(request,'cuenta.html',context)
+
+
+def actualizarCliente(request):
+    mensaje = ""
+    
+    if request.method == "POST":
+        frmCliente = ClienteForm(request.POST)
+        if frmCliente.is_valid():
+            dataCliente = frmCliente.cleaned_data
+            
+            #actualizar usuario
+            actUsuario = User.objects.get(pk=request.user.id)
+            actUsuario.first_name = dataCliente["nombre"]
+            actUsuario.last_name = dataCliente["apellidos"]
+            actUsuario.email = dataCliente["email"]
+            actUsuario.save()
+            
+            #registrar Cliente
+            nuevoCliente = Cliente()
+            nuevoCliente.usuario = actUsuario
+            nuevoCliente.direccion = dataCliente["direccion"]
+            nuevoCliente.telefono = dataCliente["telefono"]
+            nuevoCliente.save()
+            
+            mensaje = "Datos Actualizados"
+            
+    context ={
+        'mensaje':mensaje,
+        'frmCliente':frmCliente
+    }
+            
+    
+    return render(request,'cuenta.html',context)
+
+
+
+""" VISTAS PARA PROCESO DE COMPRA """
+
+@login_required(login_url='/login')
+def registrarPedido(request):
+    try:
+        clienteEditar = Cliente.objects.get(usuario = request.user)
+        
+        dataCliente = {
+            'nombre':request.user.first_name,
+            'apellidos':request.user.last_name,
+            'email':request.user.email,
+            'direccion':clienteEditar.direccion,
+            'telefono':clienteEditar.telefono,
+        }
+    except:
+         dataCliente = {
+            'nombre':request.user.first_name,
+            'apellidos':request.user.last_name,
+            'email':request.user.email
+         }
+    
+    frmCliente = ClienteForm(dataCliente)
+
+    context = {
+        'frmCliente':frmCliente
+    }
+    
+    return render(request,'pedido.html',context)
+
+
+@login_required(login_url='/login')
+def confirmarPedido(request):
+    context = {}
+    if request.method == "POST":
+        #actualizamos datos de usuario
+        actUsuario = User.objects.get(pk=request.user.id)
+        actUsuario.first_name = request.POST['nombre']
+        actUsuario.last_name = request.POST['apellidos']
+        actUsuario.save()
+        #registramos o actualizamos cliente
+        try:
+            clientePedido = Cliente.objects.get(usuario=request.user)
+            clientePedido.telefono = request.POST['telefono']
+            clientePedido.direccion = request.POST['direccion']
+            clientePedido.save()
+        except:
+            clientePedido = Cliente()
+            clientePedido.usuario = actUsuario
+            clientePedido.direccion = request.POST['direccion']
+            clientePedido.telefono = request.POST['telefono']
+            clientePedido.save()
+        #registramos nuevo pedido
+        nroPedido = ''
+        montoTotal = float(request.session.get('cartMontoTotal'))
+        nuevoPedido = Pedido()
+        nuevoPedido.cliente = clientePedido
+        nuevoPedido.save()
+        
+        #registramos el detalle del pedido
+        carritoPedido = request.session.get('cart')
+        for key,value in carritoPedido.items():
+            productoPedido = Producto.objects.get(pk=value['producto_id'])
+            detalle = PedidoDetalle()
+            detalle.pedido = nuevoPedido
+            detalle.producto = productoPedido
+            detalle.cantidad = int(value['cantidad'])
+            detalle.subtotal = float(value['subtotal'])
+            detalle.save()
+        
+        #actualizar pedido
+        nroPedido = 'PED' + nuevoPedido.fecha_registro.strftime('%Y') + str(nuevoPedido.id)
+        nuevoPedido.nro_pedido = nroPedido
+        nuevoPedido.monto_total = montoTotal
+        nuevoPedido.save()
+        
+        #registrar variable de sesion para el pedido
+        request.session['pedidoId'] = nuevoPedido.id
+        
+        # #Creamos boton de paypal
+        # paypal_dict = {
+        # "business": settings.PAYPAL_USER_EMAIL,
+        # "amount": montoTotal,
+        # "item_name": "PEDIDO CODIGO : "+ nroPedido,
+        # "invoice": nroPedido,
+        # "notify_url": request.build_absolute_uri(reverse('paypal-ipn')),
+        # "return": request.build_absolute_uri('/gracias'),
+        # "cancel_return": request.build_absolute_uri('/')
+        # }
+
+        # Create the instance.
+        # formPaypal = PayPalPaymentsForm(initial=paypal_dict)
+        
+        
+        context = {
+            'pedido':nuevoPedido,
+            # 'formPaypal':formPaypal
+        }
+        
+        #limpiamos carrito de compras
+        carrito = Cart(request)
+        carrito.clear()
+        
+    return render(request,'index.html',context)
+
+# from django.core.mail import send_mail
+
+
+# @login_required(login_url='/login')
+# def gracias(request):
+#     paypalId = request.GET.get('PayerID',None)
+#     context = {}
+#     if paypalId is not None:
+#         pedidoId = request.session.get('pedidoId')
+#         pedido = Pedido.objects.get(pk=pedidoId)
+#         pedido.estado = '1'
+#         pedido.save()
+        
+#         send_mail(
+#             'GRACIAS POR TU COMPRA',
+#             'Tu nro de pedido es ' + pedido.nro_pedido,
+#             settings.ADMIN_USER_EMAIL,
+#             [request.user.email],
+#             fail_silently=False,
+#         )
+        
+#         context = {
+#             'pedido':pedido
+#         }
+#     else:
+#         return redirect('/')
+        
+#     return render(request,'gracias.html',context)
