b52d0162bb51d943da60eeb66b300d552afd526c
Eduardo Cano <canoeduardojesus@gmail.com>
2023-04-30 03:04:39 -0300

    ULTIMO MODULO

--- elreydeltermo/settings.py
+++ elreydeltermo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ir820qpif#-y)6824%kf_+9^4@-r2&s%7&clzq0(dy68b3p&b'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -78,11 +79,11 @@ WSGI_APPLICATION = 'elreydeltermo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_edshop',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
@@ -124,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- web/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class ClienteForm(forms.Form):
-    nombre = forms.CharField(label='Nombres',max_length=200,required=True)
-    apellidos = forms.CharField(label='Apellidos',max_length=200,required=True)
-    email = forms.EmailField(label='Email',required=True)
-    direccion = forms.CharField(label='Dirección',widget=forms.Textarea)
-    telefono = forms.CharField(label='Telefono',max_length=20)
